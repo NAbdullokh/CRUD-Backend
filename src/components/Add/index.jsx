8da1a6499a7612e9193b1e1ca7a23603cbcbedcb
NAbdullokh <country.abu@gmail.com>
2022-12-02 21:02:26 +0500

    full finished project

--- src/components/Add/index.jsx
+++ src/components/Add/index.jsx
@@ -1,3 +1,4 @@
+import { getValue } from "@testing-library/user-event/dist/utils";
 import axios from "axios";
 import React, { useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
@@ -12,22 +13,36 @@ export const Add = () => {
     image: "",
   });
 
+  const getValue = (e) => {
+    setInputValue({ ...inputValue, [e.target.name]: e.target.value });
+  };
+
+  const getSave = () => {
+    axios.post(`https://travels-book.herokuapp.com/api/travel/add`, {
+      title: inputValue.title,
+      descr: inputValue.descr,
+      image: inputValue.image,
+    });
+    navigate("/");
+  };
+
   console.log(inputValue);
+
   return (
     <Container>
       <Wrap>
         <Wrap.Title>Title</Wrap.Title>
-        <Wrap.Input type={"text"} />
+        <Wrap.Input name="title" onChange={getValue} type={"text"} />
       </Wrap>
       <Wrap>
         <Wrap.Title>Decription</Wrap.Title>
-        <Wrap.Input type={"text"} />
+        <Wrap.Input name="descr" onChange={getValue} type={"text"} />
       </Wrap>
       <Wrap>
         <Wrap.Title>Image</Wrap.Title>
-        <Wrap.Input type={"text"} />
+        <Wrap.Input name="image" onChange={getValue} type={"text"} />
       </Wrap>
-      <Container.Button>Add News</Container.Button>
+      <Container.Button onClick={getSave}>Add News</Container.Button>
     </Container>
   );
 };
--- src/components/Edit/index.jsx
+++ src/components/Edit/index.jsx
@@ -6,27 +6,74 @@ import { Container, Wrap } from "../Add/style";
 export const Edit = () => {
   const navigate = useNavigate();
 
+  const { id } = useParams();
+
   const [inputValue, setInputValue] = useState({
     title: "",
     descr: "",
     image: "",
   });
 
+  const getValue = (e) => {
+    setInputValue({ ...inputValue, [e.target.name]: e.target.value });
+  };
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://travels-book.herokuapp.com/api/travel/${id.replace(":", "")}`
+      )
+      .then((res) =>
+        setInputValue({
+          title: res?.data?.travel?.title,
+          descr: res?.data?.travel?.descr,
+          image: res?.data?.travel?.image,
+        })
+      );
+  }, []);
+
+  const getUpdate = () => {
+    axios.put(
+      `https://travels-book.herokuapp.com/api/travel/${id.replace(":", "")}`,
+      {
+        title: inputValue.title,
+        descr: inputValue.descr,
+        image: inputValue.image,
+      }
+    );
+    navigate("/");
+  };
+
   return (
     <Container>
       <Wrap>
         <Wrap.Title>Title</Wrap.Title>
-        <Wrap.Input type={"text"} />
+        <Wrap.Input
+          value={inputValue.title}
+          name="title"
+          onChange={getValue}
+          type={"text"}
+        />
       </Wrap>
       <Wrap>
         <Wrap.Title>Decription</Wrap.Title>
-        <Wrap.Input type={"text"} />
+        <Wrap.Input
+          value={inputValue.descr}
+          name="descr"
+          onChange={getValue}
+          type={"text"}
+        />
       </Wrap>
       <Wrap>
         <Wrap.Title>Image</Wrap.Title>
-        <Wrap.Input type={"text"} />
+        <Wrap.Input
+          value={inputValue.image}
+          name="image"
+          onChange={getValue}
+          type={"text"}
+        />
       </Wrap>
-      <Container.Button>Add News</Container.Button>
+      <Container.Button onClick={getUpdate}>Update News</Container.Button>
     </Container>
   );
 };
